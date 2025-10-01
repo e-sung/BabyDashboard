3c54e6a37be855ea8be3f2dc671461dd8b8966c7
Sungdoo <dev.esung@gmail.com>
2025-10-01 20:56:48 +0900

    Initial Commit

---
+++ FullScreenClock/FullScreenClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullScreenClockApp.swift
+//  FullScreenClock
+//
+//  Created by 류성두 on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FullScreenClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
