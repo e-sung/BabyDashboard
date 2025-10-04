80f518254b8c332c3951df326aea815de61687b1
Sungdoo <dev.esung@gmail.com>
2025-10-05 00:22:25 +0900

    add date

--- FullScreenClock/ContentView.swift
+++ FullScreenClock/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 class ContentViewModel: ObservableObject {
     @Published var time: String = "00:00"
+    @Published var date: String = ""
 
     init() {
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
@@ -17,6 +18,7 @@ class ContentViewModel: ObservableObject {
                     .hour(.twoDigits(amPM: .omitted))
                     .minute(.twoDigits)
             )
+            self.date = Date().formatted(Date.FormatStyle(locale: Locale(identifier: "ko")).year(.defaultDigits).month(.abbreviated).day(.defaultDigits).weekday(.wide))
         })
     }
 }
@@ -26,13 +28,19 @@ struct ContentView: View {
     @ObservedObject var viewModel: ContentViewModel
 
     var body: some View {
-        Text(viewModel.time)
-            .font(.system(size: 320))
-            .lineLimit(1)
-            .minimumScaleFactor(0.1)
-            .padding()
-            .fontWeight(.bold)
-            .monospacedDigit()
+        VStack {
+            Text(viewModel.time)
+                .font(.system(size: 320))
+                .lineLimit(1)
+                .minimumScaleFactor(0.1)
+                .padding()
+                .fontWeight(.bold)
+                .monospacedDigit()
+            Text(viewModel.date)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .monospacedDigit()
+        }
     }
 }
 
