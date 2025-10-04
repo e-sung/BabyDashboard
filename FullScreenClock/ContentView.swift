8530c6531d098e0c6c484fe39dcaed7033307288
Sungdoo <dev.esung@gmail.com>
2025-10-05 00:34:36 +0900

    add last feed time

--- FullScreenClock/ContentView.swift
+++ FullScreenClock/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 class ContentViewModel: ObservableObject {
     @Published var time: String = "00:00"
     @Published var date: String = ""
+    @Published var 연두수유시간: String = "00:00"
+    @Published var 초원수유시간: String = "00:00"
 
     init() {
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
@@ -21,6 +23,14 @@ class ContentViewModel: ObservableObject {
             self.date = Date().formatted(Date.FormatStyle(locale: Locale(identifier: "ko")).year(.defaultDigits).month(.abbreviated).day(.defaultDigits).weekday(.wide))
         })
     }
+
+    func update연두수유시간() {
+        self.연두수유시간 = time
+    }
+
+    func update초원수유시간() {
+        self.초원수유시간 = time
+    }
 }
 
 
@@ -29,6 +39,21 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
+            HStack {
+                Button {
+                    viewModel.update연두수유시간()
+                } label: {
+                    Text("연두수유")
+                }
+                Spacer()
+                Button {
+                    viewModel.update초원수유시간()
+                } label: {
+                    Text("초원수유")
+                }
+            }
+            .padding()
+            Spacer()
             Text(viewModel.time)
                 .font(.system(size: 320))
                 .lineLimit(1)
@@ -40,6 +65,18 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .monospacedDigit()
+            Spacer()
+            VStack {
+                Text("최근 수유")
+                    .padding()
+                    .font(.system(size: 50))
+                VStack {
+                    Text("연두: \(viewModel.연두수유시간)")
+                    Text("초원: \(viewModel.초원수유시간)")
+                }
+                .font(.largeTitle)
+            }
+            .padding()
         }
     }
 }
