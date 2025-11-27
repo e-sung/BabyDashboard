bb2fd3b2c812f4728e3c302373acd55c135a4f0b
Sungdoo <dev.esung@gmail.com>
2025-11-27 16:40:50 +0900

    add test cases

--- BabyDashboard/SystemFeatures/AppIntents.swift
+++ BabyDashboard/SystemFeatures/AppIntents.swift
@@ -2,15 +2,6 @@ import AppIntents
 import Foundation
 import CoreData
 import Model
-import Playgrounds
-
-#Playground {
-    let formatter = DateFormatter()
-    formatter.locale = Locale(identifier: "en_US_POSIX")
-    formatter.timeZone = TimeZone(secondsFromGMT: 0)
-    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    let date = formatter.date(from: "2025-01-01 00:00:00")
-}
 
 // MARK: - Intents
 
